4e94936e44c45d3a2debcebcbe96a1afd1bbe8cd
Nico Knoll <mail@nico.is>
2023-05-31 18:37:55 +0200

    init

--- run_ubuntu.sh
+++ run_ubuntu.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run -p 8123:80 --name karaoke --mount type=bind,source=/home/karaoke/Videos,target=/mnt/videos --mount type=bind,source=/mnt/usb/,target=/mnt/usb/videos --rm karaoke & (sleep 3 && chromium-browser --kiosk http://localhost:8123)
+unclutter -idle 0.5 -root &
+docker run -p 8123:80 --name karaoke --mount type=bind,source=/home/karaoke/Videos,target=/mnt/videos --mount type=bind,source=/mnt/usb/,target=/mnt/usb/videos --rm karaoke &
+(sleep 3 && chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8123)
