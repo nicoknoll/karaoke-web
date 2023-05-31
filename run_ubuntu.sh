b9c21b520ba329874e6e20fc513b765f060bcc80
Nico Knoll <mail@nico.is>
2023-05-31 17:18:23 +0200

    init

---
+++ run_ubuntu.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -p 8123:80 --name karaoke --mount type=bind,source=/home/karaoke/Schreibtisch/videos,target=/mnt/videos --mount type=bind,source=/mnt/usb/,target=/mnt/usb/videos --rm karaoke
